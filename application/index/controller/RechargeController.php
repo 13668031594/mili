5921080afb88ba5542958ab40fd1a35f371c827d
yangyang <450428985@qq.com>
2019-06-19 16:04:02 +0800

    123

--- application/index/controller/RechargeController.php
+++ application/index/controller/RechargeController.php
@@ -80,7 +80,7 @@ class RechargeController extends \app\http\controller\IndexController
         $data = $this->class->validator_recharge1($request);
 
         $member = $this->class->member();
-        $data['total'] = '0.01';
+        $data['money'] = '0.01';
         $class = new YouyunbaoClass();
 
         $result = $class->codepay($data['money'], $data['order'], $data['type'], $member['id']);
